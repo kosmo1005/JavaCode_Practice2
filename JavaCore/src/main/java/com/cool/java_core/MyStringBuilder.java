af152ac48f635bd5e2d43333f635752dbebee6ba
Aleksey Kulushev <alexcoolarbitr@gmail.com>
2026-04-14 14:09:25 +0300

    up - add deque

--- JavaCore/src/main/java/com/cool/java_core/MyStringBuilder.java
+++ JavaCore/src/main/java/com/cool/java_core/MyStringBuilder.java
@@ -1,10 +1,11 @@
 package com.cool.java_core;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-public class MyStringBuilder  {
+public class MyStringBuilder {
     private StringBuilder sb = new StringBuilder();
-    private final Stack <String> history = new Stack<>();
+    private final Deque<String> history = new ArrayDeque<>(20);
 
     public MyStringBuilder append(String str) {
         saveSnapshot(sb);
@@ -13,12 +14,15 @@ public class MyStringBuilder  {
     }
 
     private void saveSnapshot(StringBuilder sb){
+        if (history.size() >= 20){
+            history.removeLast();
+        }
         history.push(sb.toString());
     }
 
     public void undo() {
         if (!history.isEmpty()) {
-            sb = new StringBuilder(history.pop());
+            sb = new StringBuilder(history.pollFirst());
         }
     }
 
