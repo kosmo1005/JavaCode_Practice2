c579f7883199084fc080ce1961b9ef45025cee34
Aleksey Kulushev <alexcoolarbitr@gmail.com>
2026-04-22 05:07:26 +0300

    ComplexTaskExecutor

---
+++ JavaCore/src/main/java/com/cool/java_core/mainRunners/MainComplexTaskRunner.java
@@ -0,0 +1,30 @@
+package com.cool.java_core.mainRunners;
+
+import com.cool.java_core.ComplexTaskExecutor;
+
+public class MainComplexTaskRunner {
+    public static void main(String[] args) {
+        ComplexTaskExecutor taskExecutor = new ComplexTaskExecutor();
+
+        Runnable testRunnable = () -> {
+            System.out.println(Thread.currentThread().getName() + " started the test.");
+
+            taskExecutor.executeTasks(5);
+
+            System.out.println(Thread.currentThread().getName() + " completed the test.");
+        };
+
+        Thread thread1 = new Thread(testRunnable, "TestThread-1");
+        Thread thread2 = new Thread(testRunnable, "TestThread-2");
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
